55cb387c497334fcf171593003f36800ad1e0b92
liyaSileshi <liyasileshi@gmail.com>
2020-11-29 20:56:07 -0800

    authenticated users created in webapp

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '890e039fefda66fb75b9eddbf1daedae'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' #function name of the login route, redirects user to this page if not authenticated
+login_manager.login_message_category = 'info' #bootstrap class for alert login
 
 from flaskblog import routes
